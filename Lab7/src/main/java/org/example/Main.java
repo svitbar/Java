36789ea31db252b756180874c1ddc894b7613245
svitbar <svitbar1102@gmail.com>
2023-11-23 20:01:27 +0200

    Update Lab7

--- Lab7/src/main/java/org/example/Main.java
+++ Lab7/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -30,42 +31,30 @@ public class Main {
         return sc.nextLine().split("\\s+");
     }
 
+    public static Predicate<String> isChar = str -> str
+            .chars()
+            .allMatch(Character::isLetter);
+
     public static void validateInput(String[] arr) {
         if (arr.length == 1 && arr[0].isEmpty()) {
             throw new IllegalArgumentException("Please enter a non-empty string.");
         }
 
-        for (String key : arr) {
-
-            for (int i = 0; i < key.length(); i++) {
-                if (!Character.isLetter(key.charAt(i))) {
-                   throw new IllegalArgumentException("Please use only alphabet characters.");
-                }
+        Arrays.stream(arr).forEach(w -> {
+            if (!isChar.test(w)) {
+                throw new IllegalArgumentException("Please use only alphabet characters.");
             }
-        }
+        });
     }
+
+    public static Predicate<String> isUnique = str -> str
+            .chars()
+            .distinct()
+            .count() == str.length();
 
     public static String[] wordsWithUniqueChars(String[] arr) {
         List<String> resultList = new ArrayList<>();
-
-        for (String i : arr) {
-
-            char[] word = i.toCharArray();
-            boolean unique = true;
-
-            out:
-            for (int j = 0; j < word.length; j++) {
-
-                for (int k = j + 1; k < word.length; k++) {
-                    if (word[j] == word[k]) {
-                        unique = false;
-                        break out;
-                    }
-                }
-            }
-
-            if (unique) resultList.add(i);
-        }
+        Arrays.stream(arr).filter(w -> isUnique.test(w)).forEach(resultList::add);
 
         return resultList.toArray(new String[0]);
     }
