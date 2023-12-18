fc5cd2b26cf090947741f8f1e7a3fcf351976e13
svitbar <svitbar1102@gmail.com>
2023-12-18 03:53:32 +0200

    Add task 3 for lab 10

--- Lab5/src/main/java/org/example/third/Third.java
+++ Lab5/src/main/java/org/example/third/Third.java
@@ -4,21 +4,29 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.*;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class Third {
     private static final Logger logger = LogManager.getLogger(Third.class);
-    static char k = 'k';
+    private static ResourceBundle text;
+    private static final Scanner sc = new Scanner(System.in);
+    private static final char k = 'k';
+    private static final String MENU = """
+            Please select language:
+            - en - English;
+            - ua - Українська;
+            - la - Latin.
+            """;
 
     private static String getFileName() {
-        logger.info("Please enter file name to save data in.");
-        Scanner sc = new Scanner(System.in);
+        logger.info(text.getString("enterFileName"));
         return sc.nextLine();
     }
 
     private static String getSymbolString() {
-        logger.info("Please enter a string to encrypt: ");
-        Scanner sc = new Scanner(System.in);
+        logger.info(text.getString("enterStringToEncrypt"));
         return sc.nextLine();
     }
 
@@ -27,11 +35,11 @@ public class Third {
         String s = getSymbolString();
         logger.debug("path = " + path + ", s = " + s);
         try (Writer w = new Encryptor(new FileWriter(path), k)) {
-            logger.debug("Start writing...");
+            logger.debug(text.getString("startWriting"));
             w.write(s);
-            logger.info("Successfully encrypted string and wrote it to file.");
+            logger.info(text.getString("successWriting"));
         } catch (IOException ex) {
-           logger.fatal("Failed to encrypt data: " + ex.getMessage());
+           logger.fatal(text.getString("failedWriting") + ex.getMessage());
         }
     }
 
@@ -40,7 +48,7 @@ public class Third {
         StringBuilder str = new StringBuilder();
         logger.debug("path = " + path);
         try (Reader r = new Decryptor(new FileReader(path), k)) {
-            logger.debug("Start reading...");
+            logger.debug(text.getString("startReading"));
             while (true) {
                 int c = r.read();
                 if (c == -1) break;
@@ -48,9 +56,49 @@ public class Third {
                 str.append((char) c);
             }
 
-            logger.info("Successfully decrypted string: " + str);
+            logger.info(text.getString("successReading") + str);
         } catch (IOException ex) {
-            logger.fatal("Failed to decrypt data: " + ex.getMessage());
+            logger.fatal(text.getString("failedReading") + ex.getMessage());
+        }
+    }
+
+    private static void setLocale(String language) {
+        Locale locale = new Locale(language);
+        text = ResourceBundle.getBundle("location.text", locale);
+    }
+
+    public static void menu() {
+        logger.info(MENU);
+        String cmd = sc.nextLine();
+        boolean con = true;
+
+        do {
+            switch (cmd) {
+                case "en":
+                    setLocale("en");
+                    break;
+                case "ua":
+                    setLocale("ua");
+                    break;
+                case "la":
+                    setLocale("la");
+                    break;
+                default:
+                    con = false;
+                    logger.error("Wrong command! Please try again.");
+            }
+        } while (!con);
+    }
+
+    public static void run() {
+        try {
+            menu();
+            encryptStream();
+            decryptStream();
+        } catch (RuntimeException ex) {
+            logger.fatal(ex.getMessage());
+        } finally {
+            sc.close();
         }
     }
 }
